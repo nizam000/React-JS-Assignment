46afeb8126cf218a03870ca980cf201b049e8d8e
nizam khan <vishal.kandukuri@sagarsoft.in>
2024-06-27 14:51:56 +0530

    Assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => {
+const [markdown, setMarkdown] = useState('');
+const [html, setHtml] = useState('');
+const handleInputChange = async (event) => {
+const newMarkdown = event.target.value;
+setMarkdown(newMarkdown);
+const response = await axios.post('http://localhost:5000/convert', { markdown: newMarkdown
+});
+setHtml(response.data.html);
+};
+return (
+<div className="App">
+<textarea
+value={markdown}
+onChange={handleInputChange}
+placeholder="Type your Markdown here..."
+/>
+<div
+className="preview"
+dangerouslySetInnerHTML={{ __html: html }}
+/>
+</div>
+);
+};
 export default App;
